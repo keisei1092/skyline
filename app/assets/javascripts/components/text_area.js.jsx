62bf0fec6e46c1e5fc9d14baaf133ef0a23c74c4
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-11-15 23:43:02 +0900

    [wip] config

--- app/assets/javascripts/components/text_area.js.jsx
+++ app/assets/javascripts/components/text_area.js.jsx
@@ -1,9 +1,18 @@
 var TextArea = React.createClass({
   render: function() {
     return (
-      <textarea className="main-textarea">
-        ほげほげ
+      <textarea className="main-textarea" value={this.value}>
       </textarea>
     );
   }
 });
+
+var ConfigButton = React.createClass({
+  render: function() {
+    return (
+      <div className="config-button">
+        <i className="fa fa-wrench fa-lg"></i>
+      </div>
+    )
+  }
+})
