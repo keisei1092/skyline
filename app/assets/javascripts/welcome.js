1d31fbcc317c24b74ce3d844feea41db6055274f
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-11-16 22:07:25 +0900

    add text-shadow option

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var styles = "margin: 0; border: none; width: 100%; height: 90%; color: #384158; background-color: #F2F2F2; font-family: 'YuGothic'; font-size: 16px; letter-spacing: .1em; line-height: 1.7em; box-sizing: border-box;-webkit-font-smoothing: subpixel-antialiased; text-align: justify; ";
+  var styles = "margin: 0; border: none; width: 100%; height: 90%; color: #384158; background-color: #F2F2F2; font-family: 'YuGothic'; font-size: 16px; letter-spacing: .1em; line-height: 1.7em; box-sizing: border-box;-webkit-font-smoothing: subpixel-antialiased; text-align: justify; text-shadow: 0px 0px 0px #fff; ";
   if (navigator.userAgent.indexOf('Mobile') == -1) {
     styles = styles + 'padding: 100px 200px;'
   } else {
